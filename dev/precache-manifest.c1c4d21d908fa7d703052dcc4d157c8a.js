d1b322069aad359ea7a04ea16180782e84ffc9fa
tobiasalanboyd <eboyd@udel.edu>
2020-04-17 18:39:29 -0400

    Updates

---
+++ dev/precache-manifest.c1c4d21d908fa7d703052dcc4d157c8a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/educational-game-project-team-5-eric-and-toby/devassets/citations.md"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-5-eric-and-toby/devassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/educational-game-project-team-5-eric-and-toby/devassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/educational-game-project-team-5-eric-and-toby/devassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/educational-game-project-team-5-eric-and-toby/devassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/educational-game-project-team-5-eric-and-toby/devassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "2d0229a54d07ae19ce6cf780f4d8f350",
+    "url": "/educational-game-project-team-5-eric-and-toby/devassets/sprites/aaa.png"
+  },
+  {
+    "revision": "0d0a5d3cde3a1b233e7257901b45a9b1",
+    "url": "/educational-game-project-team-5-eric-and-toby/devassets/sprites/sheep_brown.png"
+  },
+  {
+    "revision": "4fc4441a91b917a5dfcaebee0e63517f",
+    "url": "/educational-game-project-team-5-eric-and-toby/devassets/sprites/sheep_white.png"
+  },
+  {
+    "revision": "aeb391eb44d4b2e24f0fb6c9b158da5b",
+    "url": "/educational-game-project-team-5-eric-and-toby/devassets/sprites/testsprites.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-5-eric-and-toby/devfavicon.ico"
+  },
+  {
+    "revision": "7de603dee9b5eb9b7a4a",
+    "url": "/educational-game-project-team-5-eric-and-toby/devgen.main.6e5a2d14012fdcbf7ebb.bundle.js"
+  },
+  {
+    "revision": "5e69403aa42e950fedff",
+    "url": "/educational-game-project-team-5-eric-and-toby/devgen.vendors.0805bb80903235d40eaf.bundle.js"
+  },
+  {
+    "revision": "3943ba810f3831066a314b91a9bc3adb",
+    "url": "/educational-game-project-team-5-eric-and-toby/devindex.html"
+  },
+  {
+    "revision": "003187cab0ecf7a20e343e2ba6db4a34",
+    "url": "/educational-game-project-team-5-eric-and-toby/devmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/educational-game-project-team-5-eric-and-toby/devsw.js"
+  }
+]);
---
+++ dev/sw.js
@@ -0,0 +1,11 @@
+importScripts("/educational-game-project-team-5-eric-and-toby/devprecache-manifest.c1c4d21d908fa7d703052dcc4d157c8a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
