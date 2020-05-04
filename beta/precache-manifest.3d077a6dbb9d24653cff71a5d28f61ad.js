3c3871ba025aeb94f76655e727e504b1f5d173e9
tobiasalanboyd <eboyd@udel.edu>
2020-05-03 22:26:33 -0400

    Updates

---
+++ beta/precache-manifest.3d077a6dbb9d24653cff71a5d28f61ad.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaassets/citations.md"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "2d0229a54d07ae19ce6cf780f4d8f350",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaassets/sprites/aaa.png"
+  },
+  {
+    "revision": "52f6759e1c5b6d119df63d24417c681d",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaassets/sprites/backbutton.png"
+  },
+  {
+    "revision": "06898c597133ead086b28c1f48eff7ff",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaassets/sprites/button.png"
+  },
+  {
+    "revision": "48a44d0cf8a21b793e79e4df6f4ebe77",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaassets/sprites/buttons.psd"
+  },
+  {
+    "revision": "3a36ff3d9115e23fa5a9bf2fee615e3c",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaassets/sprites/forwardbutton.png"
+  },
+  {
+    "revision": "0d0a5d3cde3a1b233e7257901b45a9b1",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaassets/sprites/sheep_brown.png"
+  },
+  {
+    "revision": "4fc4441a91b917a5dfcaebee0e63517f",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaassets/sprites/sheep_white.png"
+  },
+  {
+    "revision": "058abc5eec53acff0db20aa8868c5266",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaassets/sprites/sheepies.psd"
+  },
+  {
+    "revision": "6b556e947f6f466564939e392cf2ae19",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaassets/sprites/startbutton.png"
+  },
+  {
+    "revision": "2b802de44d981a66f57d4768442604c0",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaassets/sprites/testsprites.png"
+  },
+  {
+    "revision": "b72ebe69181b84be9cee222410e13100",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaassets/sprites/tut0.png"
+  },
+  {
+    "revision": "88bb9ef32529bfb7ea8f5e73fd77d366",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaassets/sprites/tut1.png"
+  },
+  {
+    "revision": "a16f36b85154c39944eff55bb008f2ce",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaassets/sprites/tut2.png"
+  },
+  {
+    "revision": "e7d33fe4bae19f3803a64eb97f2d835e",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaassets/sprites/tut3.png"
+  },
+  {
+    "revision": "0a90bb91950cd577746d6da8274633d8",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaassets/sprites/tutbutton.png"
+  },
+  {
+    "revision": "27122cff2c3cead6baebb22591308efc",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaassets/sprites/tutsprite.png"
+  },
+  {
+    "revision": "47c31fa37e750c821b67cbba7f397901",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaassets/sprites/tutsprite.psd"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-5-eric-and-toby/betafavicon.ico"
+  },
+  {
+    "revision": "a984724c38ee95bd0726",
+    "url": "/educational-game-project-team-5-eric-and-toby/betagen.main.9ad442963a23ed3ed565.bundle.js"
+  },
+  {
+    "revision": "5e69403aa42e950fedff",
+    "url": "/educational-game-project-team-5-eric-and-toby/betagen.vendors.0805bb80903235d40eaf.bundle.js"
+  },
+  {
+    "revision": "a6d1651e277faf1ba7f73f8126b0c240",
+    "url": "/educational-game-project-team-5-eric-and-toby/betaindex.html"
+  },
+  {
+    "revision": "003187cab0ecf7a20e343e2ba6db4a34",
+    "url": "/educational-game-project-team-5-eric-and-toby/betamanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/educational-game-project-team-5-eric-and-toby/betasw.js"
+  }
+]);
---
+++ beta/sw.js
@@ -0,0 +1,11 @@
+importScripts("/educational-game-project-team-5-eric-and-toby/betaprecache-manifest.3d077a6dbb9d24653cff71a5d28f61ad.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
